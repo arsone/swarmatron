764f153e51570ca4437916e5854bbc8c2012cf0f
Adam <adsalberg@gmail.com>
2015-01-17 21:39:33 -0500

    Initial Commit

---
+++ swarmatron/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  swarmatron
+//
+//  Created by Adam Salberg on 1/17/15.
+//  Copyright (c) 2015 Adam Salberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ swarmatron/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  swarmatron
+//
+//  Created by Adam Salberg on 1/17/15.
+//  Copyright (c) 2015 Adam Salberg. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
